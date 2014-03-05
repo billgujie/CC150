234359d4eab964623cd3d4a092efe516597a4f81
GuJ <billgujie@gmail.com>
2014-03-04 23:26:55 -0800

    addnum, findLoopStart

--- src/Ch2.java
+++ src/Ch2.java
@@ -81,7 +81,72 @@ public class Ch2 {
 	}
 	
 	//2.5
-	public Node addnum(Node n1, Node n2){
-		
-	}
+	//reversed order
+    public Node addTwoNumbers(Node l1, Node l2) {
+        int c=0;
+        Node res = null;
+        Node cur = null;
+        Node i1 = l1;
+        Node i2 = l2;
+        while(i1!=null||i2!=null){
+            int num1 = i1 == null? 0 : i1.data;
+            int num2 = i2 == null? 0 : i2.data;
+            int temp = num1+num2+c;
+            if (temp>=10){c=1;}else{c=0;}
+            if (res==null){
+                res=new Node(temp%10);
+                cur=res;
+            }else{
+                cur.next=new Node(temp%10);
+                cur=cur.next;
+            }
+            i1 = i1 == null? null: i1.next;
+            i2 = i2 == null? null: i2.next;
+        }
+        if (c==1){
+            cur.next=new Node(1);
+        }
+        return res;
+    }
+    
+    //2.5
+    //follow up
+    //forward order
+    public Node addTwoNumbersFO(Node l1, Node l2) {
+    	Node res=null;
+    	//missing
+    	return res;
+    }
+    
+    //2.6
+    
+    public Node findLoopStart(Node root){
+    	//hare and rabbit
+    	Node hare = root;
+    	Node turt = root;
+    	
+    	while(hare.next!=null && turt!=null){
+    		turt=turt.next;
+    		hare=hare.next.next;
+    		if (turt==hare){
+    			break; //found meeting point
+    		}
+    	}
+    	
+    	//error checking
+    	if (turt==null||turt.next==null){
+    		return null;
+    	}
+    	
+    	hare=root;
+    	while(hare!=turt){
+    		hare=hare.next;
+    		turt=turt.next;
+    	}
+    	//found entrance
+    	return hare;
+    }
+    
+    //2.7
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,24 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Ch2 solution = new Ch2();
-		Node root= new Node(5);
-		Node a= new Node(4);
-		Node b= new Node(3);
+		Node r1= new Node(7);
+		Node r2= new Node(5);
+		Node a= new Node(1);
+		Node b= new Node(6);
 		Node c= new Node(2);
-		Node d= new Node(1);
+		Node d= new Node(9);
 		Node e= new Node(0);
 		
-		root.appendToTail(a);
-		root.appendToTail(b);
-		root.appendToTail(c);
-		root.appendToTail(d);
-		root.appendToTail(e);
+		r1.appendToTail(a);
+		r1.appendToTail(b);
+		r2.appendToTail(d);
+		r2.appendToTail(c);
+		//root.appendToTail(e);
 
-		System.out.println(root.toString());
+		System.out.println(r1.toString());
+		System.out.println(r2.toString());
 		
-		System.out.println(solution.partition(root, 2).toString());
+		System.out.println(solution.addTwoNumbers(r1, r2).toString());
 
 	}
 
